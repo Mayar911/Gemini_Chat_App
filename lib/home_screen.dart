7b331f3254dc1c59199b8c6aa78d128261c0a6a3
Mayar911 <150484075+Mayar911@users.noreply.github.com>
2025-02-12 20:01:05 +0100

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,7 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     _model = GenerativeModel(
-        model: "gemini-pro", apiKey: "AIzaSyDpCZ8HRpkAy3cQCuex39EQSWjO9rTcLp0");
+        model: "gemini-pro", apiKey: "");
     _chatSession = _model.startChat();
   }
 
